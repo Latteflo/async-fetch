e7913152e3373f15e6edca8c4d86e89a3b5e16ca
flori <sf.simion.f@gmail.com>
2023-06-26 14:57:20 +0200

    added trim() on input value

--- server-fetch/script.js
+++ server-fetch/script.js
@@ -5,6 +5,7 @@ let input = document.querySelector("input")
 button.addEventListener("click", function () {
   let dataValue = input.value
   dataValue = dataValue.charAt(0).toUpperCase() + dataValue.slice(1).toLowerCase() 
+  dataValue = dataValue.trim()
   let country = document.querySelector("#country")
   let codeCountry = country.value
   
